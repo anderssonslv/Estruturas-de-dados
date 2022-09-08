f3849eb55343329b8eae09daa96fc48ae2b57ce4
Noss1911 <89083903+anderssonslv@users.noreply.github.com>
2022-09-08 20:04:58 -0300

    Lista Encadeada Teste

--- semana5complexidade/listaEncadeada/src/main.c
+++ semana5complexidade/listaEncadeada/src/main.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-	printf("Hello World!\n");
+typedef struct ponto{
+	int num;
+	struct ponto *prox;
+}ponto;
 
-	return (0);
+int main () {
+	ponto *ini;
+	ponto *proxPonto;
+	ponto *fim;
+	ini = (ponto*) malloc (sizeof(ponto));
+	proxPonto = ini;
+
+	while ( 1 ) {
+		printf("\n1 Add\n2 Lista");
+		switch ( getchar() ) {
+			case '1':
+				printf("\nDigite n: ");
+				scanf("%d",&proxPonto->num);
+				getchar();
+
+				proxPonto->prox = (ponto*) malloc(sizeof(ponto));
+				proxPonto = proxPonto->prox;
+				fim = proxPonto;
+				proxPonto->prox = NULL;
+				break;
+			case '2':
+				proxPonto = ini;
+				while ( proxPonto != NULL ){
+					printf("[ %d ]",proxPonto->num);
+					proxPonto = proxPonto->prox;
+				}
+				proxPonto = fim;
+			default:
+				break;
+		}
+	}
+	
+
 }
