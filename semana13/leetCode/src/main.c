167813d814da2117ad058b1179d283097ebac774
Noss1911 <89083903+anderssonslv@users.noreply.github.com>
2022-11-13 16:49:00 -0300

    Número Majoritário O(n)
    Fazer um algoritmo que acha o número que mais aparece no vetor com complexidade espacial (1) e de processamento linear. Retirado do Site LeetCode.

--- semana13/leetCode/src/main.c
+++ semana13/leetCode/src/main.c
@@ -1,8 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-	printf("Hello World!\n");
+/* Problema > Achar o elemento majoritário, com complexidade de tempo linear e de espaço O(1)
+O elemento majoritário é o elemento que aparece mais ⌊n / 2⌋vezes. Você pode supor que o elemento majoritário sempre existe na matriz.
+*/
 
-	return (0);
+/* //Esse eu fiz primeiro, mas a complexidade dele ta muito alta pro requisito do exercicio
+int majorityElement (int* nums, int numsSize) {
+	int *count = (int *) calloc (4,numsSize);
+
+	for (int i = 0; i < numsSize; i++) {
+		for (int j = i+1; j < numsSize; j++){
+			if (nums[i] == nums [j]){
+				count[i] += 1;
+			}
+		}
+	}
+
+	int times = count [0], major = 0;
+
+	for (int i = 0; i < numsSize; i++) {
+		if (count[i] > times){
+			times = count[i];
+			major = i;
+		}
+	}
+
+	printf("\nMajor Number = %d, %d times",nums[major],times+1);
+
+	free (count);
+	return nums[major];
+}
+*/
+
+//E este tem uma complexidade linear em seu melhor caso
+int majorityElement (int* nums, int numsSize) {
+	int times = 1;
+
+	for (int i = 0; i < numsSize; i++) {
+		for (int j = i+1; j < numsSize; j++){
+			if (nums[i] == nums [j]){
+				times++;
+			}
+			if (times > numsSize/2)
+				return nums[i];
+		}
+	}
+}
+
+int main() {
+	int nums [5] = {1,1,1,2,7};
+
+	printf("\n%d",majorityElement(nums,5));
 }
